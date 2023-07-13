faf4398c3316b3a705bb60e52f707c68f294913b
ReyndertLindeyer <rlindeyer@hotmail.com>
2023-07-13 14:19:36 -0400

    Changes to files from completing section 332.

--- 33 - TodoList-V2/app.js
+++ 33 - TodoList-V2/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const _= require('lodash');
 
 const app = express();
 
@@ -44,6 +45,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = new mongoose.Schema ({
+  name : String,
+  items: [itemsSchema]
+})
+
+const List = mongoose.model("List", listSchema);
+
 //Item.insertMany(defaultItems);
 
 app.get("/", function(req, res) {
@@ -69,40 +77,75 @@ app.get("/", function(req, res) {
 
 });
 
-app.post("/", function(req, res){
+app.get('/:customListName', function(req , res){
+  //To make sure the first letter is always capitalized
+  const customListName = _.capitalize(req.params.customListName);
 
-  //Saving the item passed
-  const newItem = req.body.newItem;
-  const item = new Item({
-    name: newItem
+  List.findOne({name: customListName}).exec().then((foundList) =>{
+    //console.log(foundList);
+    //if not found a document with that name then we want to create this document only once! 
+    if(!foundList){ 
+      const list = new List({
+        name: customListName,
+        items: defaultItems
+      });
+      //save into the database
+      list.save();
+      //Redirecting to reload the page so that the user will see the new items
+      res.redirect("/"+customListName);
+    }
+    else{ //if found a document in the List collection with the specified name
+      res.render("list", {listTitle: foundList.name, newListItems: foundList.items})
+    }
+  }).catch((error)=>{
+    console.log(error);
   });
 
-  item.save().then(()=>{
-    console.log("Succesfully saved new Item to DB.")
-  }).catch((error) =>{
-    console.error("There was an error inserting into DB: ", error)
-  });;
-  //Redirecting to reload the page so that the user will see the new items
-  res.redirect('/');
+});
+
+app.post("/", function(req, res){
+ 
+  const itemName = req.body.newItem;
+  const listName = req.body.list;//list declared in the list.ejs file
+  const newItem = new Item({
+    name: itemName
+  });
+  if(listName==="Today"){
+    //will save our item into the collection of Item
+    newItem.save(); 
+    //Redirecting to reload the page so that the user will see the new items
+    res.redirect("/");
+  }
+  else{
+    List.findOne({name: listName}).exec().then((foundList) =>{
+      foundList.items.push(newItem);
+      foundList.save();
+      res.redirect("/"+ listName);
+    });
+  }
 });
 
 app.post("/delete", function(req, res){
   
   const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;
   
-  //Removing the item with this id
-  Item.findByIdAndRemove({_id: checkedItemID}).exec().then((foundItem)=>{
-    console.log("Succesfully removed the item from Items collection");
-  }).catch((error)=>{
-    console.error(error);
-  });
-
-  //Redirecting to reload the page so that the user will see the new items
-  res.redirect('/');
-});
-
-app.get("/work", function(req,res){
-  //res.render("list", {listTitle: "Work List", newListItems: workItems});
+  if(listName==="Today"){
+    Item.findByIdAndRemove({_id: checkedItemID}).exec().then((foundItem)=>{
+      console.log("Succesfully removed the item from Items collection");
+    }).catch((error)=>{
+      console.error(error);
+    });
+    //Redirecting to reload the page so that the user will see the new items
+    res.redirect("/");
+  }
+  else{
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemID}}}).exec().then((foundList)=>{
+      res.redirect("/"+ listName);
+    }).catch((error)=>{
+      console.error(error);
+    });
+  }
 });
 
 app.get("/about", function(req, res){
