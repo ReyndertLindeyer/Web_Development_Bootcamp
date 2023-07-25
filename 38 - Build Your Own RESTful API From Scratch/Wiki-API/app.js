dd90432c7038b7b5939898bfbeea21db77e66e0f
ReyndertLindeyer <rlindeyer@hotmail.com>
2023-07-25 09:04:43 -0400

    Completed additional work on section 38

--- 38 - Build Your Own RESTful API From Scratch/Wiki-API/app.js
+++ 38 - Build Your Own RESTful API From Scratch/Wiki-API/app.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const _= require('lodash');
+const _ = require('lodash');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // MongoDB connection URL
@@ -17,9 +17,70 @@ const mongoURL = process.env.mongoURL;
 
 // Connect to MongoDB
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+//Create MongoDB schema
+const articlesSchema = new mongoose.Schema({
+    title: String,
+    content: String
+})
+
+//Compiling schema into a model
+const Article = mongoose.model("articles", articlesSchema);
+
+app.route("/articles")
+    .get(
+        function (req, res) {
+            Article.find().then((foundArticles) => {
+                res.send(foundArticles);
+            }).catch((e) => {
+                console.log(e);
+            });
+        })
+    .post(
+        function (req, res) {
+            const newArticle = new Article({
+                title: req.body.title,
+                content: req.body.content
+            });
+            newArticle.save().then(() => {
+                console.log("Created article successfully.");
+                res.send(newArticle);
+            }).catch(err => {
+                console.log(err);
+            })
+        })
+    .delete(
+        function (req, res) {
+            Article.deleteMany({});
+            res.send("Successfully deleted all articles!");
+        });
+
+app.route("/articles/:articleName")
+    .get(
+        function (req, res) {
+            const searchedTitle = req.params.articleName;
+            Article.findOne({ title: searchedTitle }).then(foundArticle => {
+                res.send(foundArticle);
+            }).catch((e) => {
+                console.log(e);
+            });
+        })
+    .put(
+        function (req, res) {
+            const searchedTitle = req.params.articleName;
+            Article.findOne({ title: searchedTitle }).then(foundArticle => {
+                res.send(foundArticle);
+            }).catch((e) => {
+                console.log(e);
+            });
+        });
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
